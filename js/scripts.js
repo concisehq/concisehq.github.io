34cd89f1b8535eb2709d93773a0cc593c9afe876
Shashank <shashankp64@users.noreply.github.com>
2023-12-22 23:16:02 +0530

    fixed waitlist form

--- js/scripts.js
+++ js/scripts.js
@@ -5,3 +5,22 @@
 */
 // This file is intentionally blank
 // Use this file to add JavaScript to your project
+
+function joinWaitlist(input) {
+    var email = document.getElementById(input).value;
+    if (validateEmail(email)) {
+      var url = 'https://app.concise.live/waitlist?email=' + encodeURIComponent(email);
+      fetch(url)
+        .then(response => response.text())
+        .then(data => console.log(data))
+        .catch(error => console.error('Error:', error));
+    } else {
+      alert('Please enter a valid email address.');
+    }
+  }
+  
+  function validateEmail(email) {
+    var emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
+    return emailRegex.test(email);
+  }
+  
